11d7ac950602086921189829363bf8bdf97ae08a
Nikita <nrw@narola.email>
2023-09-27 16:46:44 +0530

    add exception

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,8 +1,12 @@
 package com.example.demo.exception;
 
+
 import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.ControllerAdvice;
+
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
