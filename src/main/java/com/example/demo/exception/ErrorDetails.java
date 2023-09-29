8c906ed4ad229d9603ccd2d845de28b5f8b9fbe2
Nikita <nrw@narola.email>
2023-09-29 09:59:30 +0530

    add exception

--- src/main/java/com/example/demo/exception/ErrorDetails.java
+++ src/main/java/com/example/demo/exception/ErrorDetails.java
@@ -10,4 +10,11 @@ public class ErrorDetails {
         this.message = message;
     }
 
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
