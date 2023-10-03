3f13d58bc2a5d5b85b7f29002f200d490c57cc36
Nikita <nrw@narola.email>
2023-10-03 09:08:03 +0530

    add exception

--- src/main/java/com/example/demo/validation/StoreValidator.java
+++ src/main/java/com/example/demo/validation/StoreValidator.java
@@ -18,16 +18,16 @@ public class StoreValidator implements Validator {
     public void validate(Object target, Errors errors) {
         StoreDTO storeDTO = (StoreDTO) target;
 
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "storeStatus", "required.field", "Store status is required.");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userId", "required.field", "User ID is required.");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "required.field", "Name is required.");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "required.field", "Email is required.");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "title", "required.field", "Title is required.");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "iconPath", "required.field", "Icon path is required.");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "storeAddress", "required.field", "Store address is required.");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "storyTitle", "required.field", "Story title is required.");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "storyDescription", "required.field", "Story description is required.");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "messageToBuyers", "required.field", "Message to buyers is required.");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "storeStatus", "required.field");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userId", "required.field");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "required.field");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "required.field");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "title", "required.field");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "iconPath", "required.field");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "storeAddress", "required.field");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "storyTitle", "required.field");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "storyDescription", "required.field");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "messageToBuyers", "required.field");
 
         if (storeDTO.getStoreStatus() != null && storeDTO.getStoreStatus().length() > 15) {
             errors.rejectValue("storeStatus", "field.toolong", "Store status must not exceed 15 characters.");
