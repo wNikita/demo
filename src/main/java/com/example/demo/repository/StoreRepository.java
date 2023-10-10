67c481304128279c21a81af6f9fd80f57aeba5c7
Nikita <nrw@narola.email>
2023-10-10 12:22:34 +0530

    Changes

--- src/main/java/com/example/demo/repository/StoreRepository.java
+++ src/main/java/com/example/demo/repository/StoreRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.model.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
--- src/main/java/com/example/demo/service/StoreService.java
+++ src/main/java/com/example/demo/service/StoreService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
