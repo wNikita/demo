5c7de6b05632751b8da4abdfdd68163863147ec9
Nikita <nrw@narola.email>
2023-10-10 10:50:00 +0530

    Changes

--- src/main/java/com/example/demo/repository/StoreRepository.java
+++ src/main/java/com/example/demo/repository/StoreRepository.java
@@ -3,7 +3,6 @@ package com.example.demo.repository;
 import com.example.demo.model.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
