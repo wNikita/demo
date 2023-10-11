cc4dea5539aa3db9cd13427e10997b147429b4d6
Nikita <nrw@narola.email>
2023-10-11 11:49:08 +0530

    add delete API

--- src/main/java/com/example/demo/exception/ErrorDetails.java
+++ src/main/java/com/example/demo/exception/ErrorDetails.java
@@ -2,8 +2,6 @@ package com.example.demo.exception;
 
 public class ErrorDetails {
     private String message;
-
-
     public ErrorDetails(String message) {
         super();
         this.message = message;
