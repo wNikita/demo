d9fcdb7e0e0a1c2fd4ba9c03f0900835215187c2
Nikita <nrw@narola.email>
2023-10-18 18:02:21 +0530

    updates

--- src/main/java/com/example/demo/service/StoreService.java
+++ src/main/java/com/example/demo/service/StoreService.java
@@ -69,7 +69,6 @@ public class StoreService {
         store.setStoreStatus(updatedStore.getStoreStatus());
         store.setName(updatedStore.getName());
         store.setEmail(updatedStore.getEmail());
-        store.setUserId(updatedStore.getUserId());
         store.setTitle(updatedStore.getTitle());
         store.setIconPath(updatedStore.getIconPath());
         store.setStoreAddress(updatedStore.getStoreAddress());
